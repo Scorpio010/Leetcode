33572902a44b9af8f0cf4c1186d2e748ecbab9c1
Scorpio010 <74903765+Scorpio010@users.noreply.github.com>
2021-03-24 20:43:04 +0800

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,29 +1,27 @@
-package Homework.Leetcode.p21;
+package leetcode.p94;
 
+import java.util.ArrayList;
 import java.util.List;
-
 public class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-      ListNode cur1 = l1;
-      ListNode cur2 = l2;
-      ListNode last = new ListNode(-1);
-      ListNode newhead = last;
-      while(cur1!=null&&cur2!=null){
-          if (cur1.val<=cur2.val){
-              last.next = cur1;
-              last = cur1;
-              cur1 = cur1.next;
-          }else{
-              last.next = cur2;
-              last = cur2;
-              cur2 = cur2.next;
-          }
-      }
-      if (cur1!=null){
-          last.next = cur1;
-      }else if (cur2!=null){
-          last.next = cur2;
-      }
-      return newhead.next;
+    public List<Integer> inorderTraversal(TreeNode root) {
+         inorderList.clear();
+         inorder(root);
+         return inorderList;
+    }
+    private List<Integer> inorderList = new ArrayList<>();
+    private void inorder(TreeNode root){
+        if (root == null){
+            return;
+        }
+        inorder(root.left);
+        inorderList.add(root.val);
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        solution.inorderTraversal(null);
+        solution.inorderTraversal(null);
     }
 }
