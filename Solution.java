97604fbe8ca0ed285276be573f4320acaeb0fa8e
Scorpio010 <74903765+Scorpio010@users.noreply.github.com>
2021-03-25 10:26:06 +0800

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,22 +1,29 @@
-package leetcode.p572;
-
-public class Solution {
-    //判断是否为另一个树的子树
-    public boolean isSubtree(TreeNode s, TreeNode t) {
-     if (t==null){
-         return true;
-     }
-     if (s==null){
-         return false;
-     }
-     return isSubtree(s.left,t)||isSubtree(s.right,t)||isSametree(s,t);
+package leetcode.p110;
+public class TreeNode {
+    public int val;
+    public leetcode.p94.TreeNode left;
+    public leetcode.p94.TreeNode right;
+    public TreeNode() {}
+    public TreeNode(int val) { this.val = val; }
+    public TreeNode(int val, leetcode.p94.TreeNode left, leetcode.p94.TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
-    public boolean isSametree(TreeNode s, TreeNode t){
-        if (s==null&&t==null){
-            return true;
-        }
-        if (s==null||t==null) return false;
-        if (s.val!=t.val) return false;
-        return isSametree(s.left,t.left)&&isSametree(s.right,t.right);
+}
+public class Solution {
+    //判断是否为平衡二叉树
+    public boolean isBalanced(TreeNode root) {
+       if (getheight(root)>=0) return true;
+       else return false;
+
+    }
+    public int getheight(TreeNode root){
+        if (root==null) return 0;
+        int leftsubheight = getheight(root.left);
+        int rightsubheight = getheight(root.right);
+        if (leftsubheight>=0&&rightsubheight>=0&&Math.abs(leftsubheight-rightsubheight)<=1)
+        return Integer.max(getheight(root.left),getheight(root.right))+1;
+        else return -1;
     }
 }
