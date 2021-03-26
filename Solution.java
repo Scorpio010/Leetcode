56b46ecce53eb89389a3d80c86226f16c5cc17bb
Scorpio010 <74903765+Scorpio010@users.noreply.github.com>
2021-03-26 15:29:09 +0800

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,29 +1,27 @@
-package leetcode.p110;
-public class TreeNode {
-    public int val;
-    public leetcode.p94.TreeNode left;
-    public leetcode.p94.TreeNode right;
-    public TreeNode() {}
-    public TreeNode(int val) { this.val = val; }
-    public TreeNode(int val, leetcode.p94.TreeNode left, leetcode.p94.TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
+package leetcode.p101;
+ class TreeNode {
+   public int val;
+   public TreeNode left;
+   public TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
 public class Solution {
-    //判断是否为平衡二叉树
-    public boolean isBalanced(TreeNode root) {
-       if (getheight(root)>=0) return true;
-       else return false;
-
+    public boolean isSymmetric(TreeNode root) {
+          return ismirrorTree(root,root);
     }
-    public int getheight(TreeNode root){
-        if (root==null) return 0;
-        int leftsubheight = getheight(root.left);
-        int rightsubheight = getheight(root.right);
-        if (leftsubheight>=0&&rightsubheight>=0&&Math.abs(leftsubheight-rightsubheight)<=1)
-        return Integer.max(getheight(root.left),getheight(root.right))+1;
-        else return -1;
+    public boolean ismirrorTree(TreeNode p,TreeNode q){
+        if (p==null&&q==null){
+            return true;
+        }
+        if (p==null||q==null){
+            return false;
+        }
+       return p.val==q.val&&ismirrorTree(p.left,q.right)&&ismirrorTree(p.right,q.left);
     }
 }
