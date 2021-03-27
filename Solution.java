b9b68a98a70ba3eb1a2c8b30547b9004b5bdbd73
Scorpio010 <74903765+Scorpio010@users.noreply.github.com>
2021-03-27 16:43:44 +0800

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,27 +1,41 @@
-package leetcode.p101;
- class TreeNode {
-   public int val;
-   public TreeNode left;
-   public TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
-  }
-public class Solution {
-    public boolean isSymmetric(TreeNode root) {
-          return ismirrorTree(root,root);
+package leetcode.p236;
+
+import java.util.Stack;
+
+public class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+ }
+ public class Solution{
+    public  TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q){
+        if (root==p||root==q) return root;
+        if (root==null) return null;
+        TreeNode left  = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+        if (left!=null&&right!=null) return root;
+        else if (left!=null) return left;
+        else if (right!=null) return right;
+        else return null;
     }
-    public boolean ismirrorTree(TreeNode p,TreeNode q){
-        if (p==null&&q==null){
-            return true;
+ }
+public class Solution2 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+         if (root==p||root==q) return root;
+         if (root==null) return null;
+         TreeNode cur=root;
+         TreeNode last = null;
+        Stack<TreeNode> stack = new Stack<>();
+        while(cur!=null||!stack.isEmpty()){
+            while(cur!=null){
+                stack.push(cur);
+                cur = cur.left;
+            }
         }
-        if (p==null||q==null){
-            return false;
+        TreeNode pop = stack.peek();
+        if (pop.right==null||pop.right==last){
+
         }
-       return p.val==q.val&&ismirrorTree(p.left,q.right)&&ismirrorTree(p.right,q.left);
     }
 }
